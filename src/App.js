cbd3f0764f840c92c3968aeaf87182056e627f8d
chaggysen <sen.wang@mail.mcgill.ca>
2020-05-28 19:17:36 -0400

    first commit(setup files)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch></Switch>
+      {/* If the route is the home page, the component i want to show is Home
+      The same logic applies to other routes */}
+      {/* We need to add exact to path because with React Router DOM, it will only match the first path.
+      For example, /rooms(rooms page) also contains / (home page). When we add excat, only when the path matchs exactly, 
+      then it will render the path page*/}
+      {/* We have multiple types of single-room (economy, presidential). We need to use the route parameters
+      In order to set up the route parameters, we need to setup the /:[variable_name]. Later on, when we will be working on this single room component,
+      we will need to access the [variable_name] variable and pull the specific informations about that specific page*/}
+      {/* Finally, we can set up the switch component. To set up the Route for the Error component.
+      In this case, we don't need a path*/}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
