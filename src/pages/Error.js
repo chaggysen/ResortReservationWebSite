54382897290dcb68ada5f232ba0106d894c95792
chaggysen <sen.wang@mail.mcgill.ca>
2020-05-29 13:47:21 -0400

    banner component for rooms page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,6 +4,7 @@ import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 
 export default function Error() {
+  // The banner is the children of hero and the link is the children of banner
   return (
     <Hero>
       <Banner title="404" subtitle="page not found">
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 const Rooms = () => {
   // Here we don't want to use the default className. So we need to pass down the hero props we want
   // This will overwrite the default one we setup
-  return <Hero hero="roomsHero" />;
+  return (
+    <Hero hero="roomsHero">
+      <Banner title="our rooms">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Rooms;
