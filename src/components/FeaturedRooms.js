3319cc6a4a73cb0960fef74318610058f37ac858
chaggysen <sen.wang@mail.mcgill.ca>
2020-05-30 14:24:46 -0400

    setup loading and room component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
 import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
 
 export default class FeaturedRooms extends Component {
   // The contextType property on a class can be assigned a Context object created by React.createContext().
   // This let you consume the nearest current value of that Context type using this.context. You can reference this in any lifecycle methods including the render function
   static contextType = RoomContext;
   render() {
-    //   The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables
+    // The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables
     // Here, we are accessing the values from featuredRooms props and we rename it rooms from this.context(RoomContext)
-    const { featuredRooms: rooms } = this.context;
+    // We also have access to loading prop
+    let { loading, featuredRooms: rooms } = this.context;
+
+    // Here we display the room component. Only featured rooms here
+    rooms = rooms.map((room) => {
+      return <Room key={room.id} room={room} />;
+    });
+
     return (
-      <div>
-        <Loading />
-      </div>
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {/* Here, if loading is true, we display the loading component. Else, we display the room component */}
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
     );
   }
 }
